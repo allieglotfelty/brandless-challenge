c26168cb40f99d44b24a8e678e30edab222952be
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-16 17:23:10 +0000

    Built basic Flask app in server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, session, request
+
+app = Flask(__name__)
+
+app.secret_key = 'asldfjajeiownasldknvauwaeadsfanadnvaoqpndcsdf'
+
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def display_homepage():
+    """Displays homepage."""
+
+    return render_template('homepage.html')
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
