e1ac0328c2aa2b4748860d393cea3c04e9efc758
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-16 17:54:17 +0000

    Added connect_to_twitter function to access the Twitter API and initialized to test printing my account's public tweets

---
+++ server_utilities.py
@@ -0,0 +1,19 @@
+import os
+import tweepy
+
+def connect_to_twitter(twitter_handle):
+    """Access the Twitter API using authentication"""
+
+    consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+    consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+    access_token_key = os.environ['TWITTER_ACCESS_TOKEN']
+    access_token_secret = os.environ['TWITTER_TOKEN_SECRET']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token_key, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print tweet.text
