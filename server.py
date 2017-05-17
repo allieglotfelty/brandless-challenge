48dcd5c4aa764aa8656774a078c5c262650923f1
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-17 18:11:16 +0000

    Got /markovify route working with text from tweets and imported jsonify

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, jsonify, session, request
 from jinja2 import StrictUndefined
 import server_utilities
+
 
 app = Flask(__name__)
 
@@ -15,11 +16,14 @@ def display_homepage():
 
     return render_template('homepage.html')
 
-@app.route('/markovify')
+
+@app.route('/markovify', methods=["GET"])
 def create_markov_tweet():
     """Creates Markov tweet based on tweets from the given Twitter handle."""
 
+    print "I'm here"
     twitter_handle = request.args.get('twitter-handle')
+    print twitter_handle
     tweet_text = server_utilities.connect_to_twitter(twitter_handle)
 
     return jsonify(tweet_text)
