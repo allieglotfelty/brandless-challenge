65e97857b856c5b44fc3e8d46c08eb621325ff96
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-17 22:19:49 +0000

    Added alert if the twitter user does not exist

--- static/markov.js
+++ static/markov.js
@@ -3,9 +3,13 @@ $(document).ready(function() {
     var generateTweetButton = document.getElementById("generate-tweet");
 
     function displayTweet(results) {
-        var handle = results["twitter_handle"];
-        var tweet = results["new_tweet"];
-        alert(handle + ": " + tweet);
+        if (results === "Sorry, this user does not exist. Try again.") {
+            alert(results);
+        } else {
+            var handle = results["twitter_handle"];
+            var tweet = results["new_tweet"];
+            alert(handle + ": " + tweet);
+        }
     }
 
     function generateTweetText() {
