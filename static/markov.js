a6c8f5443d98d2120dff8c5215a0ec11633ecf65
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-17 22:12:59 +0000

    Displayed markov tweet as an alert box

--- static/markov.js
+++ static/markov.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
 
     var generateTweetButton = document.getElementById("generate-tweet");
 
+    function displayTweet(results) {
+        var handle = results["twitter_handle"];
+        var tweet = results["new_tweet"];
+        alert(handle + ": " + tweet);
+    }
+
     function generateTweetText() {
         var twitterHandle = document.getElementById("twitter-handle").value;
         $.get("/markovify", {"twitter-handle": twitterHandle}, displayTweet);
-    }
-
-    function displayTweet(results) {
-        console.log(results);
     }
 
     generateTweetButton.onclick = generateTweetText;
