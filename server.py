1d147b4195f89574ec442ba1647de7d917b8af87
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-17 23:57:49 +0000

    Updated markovify route to take in new markov class

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, session, request
 from jinja2 import StrictUndefined
 import server_utilities
-import markov
+from markov import Markov
 
 app = Flask(__name__)
 
@@ -21,13 +21,19 @@ def create_markov_tweet():
     """Creates Markov tweet based on tweets from the given Twitter handle."""
 
     twitter_handle = request.args.get('twitter-handle')
+    # try:
+    #     tweet_text = server_utilities.connect_to_twitter(twitter_handle)
+    # except Exception:
+    #     return jsonify("Sorry, this user does not exist. Try again.")
+
     try:
-        tweet_text = server_utilities.connect_to_twitter(twitter_handle)
+        twitter_handle_markov = Markov(twitter_handle, 2)
     except Exception:
         return jsonify("Sorry, this user does not exist. Try again.")
 
-    tweet_chains = markov.make_n_gram_chains(tweet_text, 2)
-    new_tweet = markov.make_a_markov_phrase(tweet_chains)
+    new_tweet = twitter_handle_markov.make_markov_tweet()
+    # tweet_chains = markov.make_n_gram_chains(tweet_text, 5)
+    # new_tweet = markov.make_a_markov_phrase(tweet_chains)
     results = {"new_tweet": new_tweet, "twitter_handle": twitter_handle}
     session[twitter_handle] = session.get(twitter_handle, []) + [new_tweet]
 
